343d3db762bc5e5e1b76ff09c8868d2d30f8f23c
Varun Vora <varunvora@live.com>
2018-04-27 00:46:50 +0530

    Added file for all test cases

---
+++ test/all_cases.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+struct struct_name1 {
+	int member1;
+	float member2;
+};
+
+int main()
+{
+	int i,j,k;
+
+	struct struct_name1 struct_id;
+	struct_id.member1 = 18*23;
+
+	for (i=0; i != 12; i++)
+	{
+		32*12;
+		for(j=0; j >=6*8; j-=2)
+		{
+			k = i < j ? j : i > 0 ? i : 0;
+			
+		}
+	}
+}
