5c058d82ddcb5a3adc37a64ac1e8f30c7aeb0bdf
Vivek Kapoor <vivek.kapoor.2959@gmail.com>
2018-04-27 09:19:22 +0530

    pretty

--- run.sh
+++ run.sh
@@ -1,16 +1,20 @@
+yacc -d symbol_table.y
+lex symbol_table.l
+gcc -g y.tab.c lex.yy.c -ll -o SymbolTable
+clear
+yacc -d AST.y
+lex AST.l
+gcc -g y.tab.c lex.yy.c graph.c -ll -o AST
+clear
+yacc -d ICG.y
+lex ICG.l
+gcc -g y.tab.c lex.yy.c -ll -o ICG
+clear
 echo "---------------"
 echo "MINI C COMPILER"
 echo "---------------"
 echo "Supported constructs : For loop, Ternary Operators and Structures"
-yacc -d symbol_table.y
-lex symbol_table.l
-gcc -g y.tab.c lex.yy.c -ll -o SymbolTable
-yacc -d AST.y
-lex AST.l
-gcc -g y.tab.c lex.yy.c graph.c -ll -o AST
-yacc -d ICG.y
-lex ICG.l
-gcc -g y.tab.c lex.yy.c -ll -o ICG
+
 echo "Use ./SymbolTable < filename to generate symbol table"
 echo "Use ./AST < filename to generate abstract syntax tree"
 echo "Use ./ICG < filename to generate intermediate code"
